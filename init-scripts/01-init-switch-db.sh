715a6a920de5a22d70c2c998663eafd102f3af1b
kayp514 <pa.ka12@outlook.com>
2025-03-27 16:43:31 -0400

    Refactor database initialization script: Simplify user and database creation for FreeSWITCH, removing unnecessary role and schema commands while ensuring proper privileges are granted.

--- init-scripts/01-init-switch-db.sh
+++ init-scripts/01-init-switch-db.sh
@@ -1,34 +1,19 @@
 #!/bin/bash
 set -e
 
-# Set default password if not provided
-database_password=${DATABASE_PASSWORD:-insecure12345}
-
+# Create FreeSWITCH user and database
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    -- Create FreeSWITCH database
-    CREATE DATABASE freeswitch;
+    -- Create FreeSWITCH user
+    CREATE USER freeswitch WITH PASSWORD '$SWITCH_DB_PASSWORD';
 
-    -- Create FreeSWITCH role with superuser privileges
-    CREATE ROLE freeswitch WITH SUPERUSER LOGIN PASSWORD '$database_password';
+    -- Create FreeSWITCH database
+    CREATE DATABASE freeswitch OWNER freeswitch;
+
+    -- Connect to FreeSWITCH database
+    \c freeswitch
 
     -- Grant privileges
     GRANT ALL PRIVILEGES ON DATABASE freeswitch TO freeswitch;
-
-    -- Connect to freeswitch database
-    \c freeswitch
-
-    -- Create uuid-ossp extension
-    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-    -- Create schema for FreeSWITCH
-    CREATE SCHEMA IF NOT EXISTS freeswitch;
-
-    -- Grant schema privileges to freeswitch role
-    GRANT ALL ON SCHEMA freeswitch TO freeswitch;
-
-    -- Set search path
-    ALTER DATABASE freeswitch SET search_path TO freeswitch,public;
-
-    -- Set schema owner
-    ALTER SCHEMA freeswitch OWNER TO freeswitch;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO freeswitch;
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO freeswitch;
 EOSQL 
